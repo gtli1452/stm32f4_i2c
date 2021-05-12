9d71c6170a21c42d096d98240c00cfce5af3e1df
Tim.Li <silentbrain1452@gmail.com>
2021-05-12 11:17:18 +0800

    Config GPIO for spi
    For SPI :
    Configure PC3 output for /CS, PC1 output for SDO, PC15 output for SCLK,
    PC13 input for SDI.
    
    For ATE320:
    Configure PC14 output for /RST, PA01 input for /BUSY.

--- Src/main.c
+++ Src/main.c
@@ -189,12 +189,35 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, _RST_Pin|SCLK_Pin|SDO_Pin|_CS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, SCL_Pin|SDA_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : SDI_Pin */
+  GPIO_InitStruct.Pin = SDI_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(SDI_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : _RST_Pin SCLK_Pin SDO_Pin _CS_Pin */
+  GPIO_InitStruct.Pin = _RST_Pin|SCLK_Pin|SDO_Pin|_CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : _BUSY_Pin */
+  GPIO_InitStruct.Pin = _BUSY_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(_BUSY_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : SCL_Pin */
   GPIO_InitStruct.Pin = SCL_Pin;
@@ -206,7 +229,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : SDA_Pin */
   GPIO_InitStruct.Pin = SDA_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
   HAL_GPIO_Init(SDA_GPIO_Port, &GPIO_InitStruct);
 
